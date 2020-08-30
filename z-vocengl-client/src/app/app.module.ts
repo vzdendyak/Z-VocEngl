bf7a6c647d301d8b2cab606c19de11c6f9161f03
Vasyl <vzdendyak@gmail.com>
2020-08-31 01:05:26 +0300

    Added search form

--- z-vocengl-client/src/app/app.module.ts
+++ z-vocengl-client/src/app/app.module.ts
@@ -8,6 +8,7 @@ import {WordPageModule} from './word/word-page.module';
 import {RouterModule} from '@angular/router';
 import {WordDetailsComponent} from './word/components/word-details/word-details.component';
 import {HttpClientModule} from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import {HttpClientModule} from '@angular/common/http';
     RouterModule.forRoot([
       // {path: '',  redirectTo: 'word/0', pathMatch: 'full'},
       {path: 'word/:wordId', component: WordDetailsComponent}
-    ])
+    ]),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
